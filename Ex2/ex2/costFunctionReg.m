2240ca07d266f70aa68f57e215f3b17994d8f5e9
JaewoongShin <flyinggiggs@gmail.com>
2018-03-26 00:43:45 -0400

    Added ex2 via upload
    Added my solution codes for ex2 assignment.

---
+++ Ex2/ex2/costFunctionReg.m
@@ -0,0 +1,37 @@
+function [J, grad] = costFunctionReg(theta, X, y, lambda)
+%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+%   theta as the parameter for regularized logistic regression and the
+%   gradient of the cost w.r.t. to the parameters. 
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+onesY = ones(m,1);
+onesT = ones(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+
+utheta = theta;
+
+% Because we do not want to penalize theta0
+utheta(1, 1) = 0;
+
+% Cost function :
+% 1/m * sigma 1 to m [-y log(h(x)) - (1-y)log(1-h(x)) + lambda * theta^2]
+J = 1/m * (-(y'*log(sigmoid(X*theta))) - ((onesY.-y)'*log(onesY.-sigmoid(X*theta))) +(lambda / 2 * onesT' * utheta.^2));
+ 
+% Gradient of the cost
+grad = 1/m  * ((X' * (sigmoid(X * theta).-y)) + (lambda * utheta));
+
+
+% =============================================================
+
+end
